824769170429ba8d76f5b7c9f5f7e9c3f43fb4c6
Thuat Ha <thuat.ha@vServer.com>
2021-06-15 18:42:30 +0700

    update add chidren form

--- src/pages/settingProfile.vue
+++ src/pages/settingProfile.vue
@@ -4,9 +4,9 @@
       <font-awesome-icon class="icon-star" :icon="['fas', 'chevron-left']" />
       <p>back</p>
     </div>
-    <el-container style="width: 1045px; height: 100vh; border: 1px solid #eee">
+    <el-container style="width: 1070px; height: 100vh; border: 1px solid #eee">
       <el-aside style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['1', '3']">
+        <el-menu :default-openeds="['1', '5']">
           <el-menu-item-group>
             <el-menu-item index="1">Profile setting</el-menu-item>
             <el-menu-item index="2">Children</el-menu-item>
--- src/profiles/settingChildren.vue
+++ src/profiles/settingChildren.vue
@@ -5,10 +5,8 @@
         class="avatar-uploader"
         action="https://jsonplaceholder.typicode.com/posts/"
         :show-file-list="false"
-        :on-success="handleAvatarSuccess"
-        :before-upload="beforeAvatarUpload"
       >
-        <el-avatar :size="83" :src="circleUrl">
+        <el-avatar :size="83">
           <font-awesome-icon class="icon-star" :icon="['fas', 'camera']" />
         </el-avatar>
       </el-upload>
@@ -38,67 +36,95 @@
         </el-form-item>
       </div>
       <div class="group">
+        <!-- search -->
         <el-form-item label="Interests">
           <el-input
-            style="width: 703px"
+            style="width: 727px"
             placeholder="Search"
             suffix-icon="el-icon-search"
-            v-model="sizeForm.search"
-          >
-          </el-input>
-          <el-tag
-            :key="tag"
-            v-for="tag in dynamicTags"
-            closable
-            :disable-transitions="false"
-            @close="handleClose(tag)"
-          >
-            {{ tag }}
-          </el-tag>
-          <el-input
             class="input-new-tag"
-            v-if="inputVisible"
-            v-model="inputValue"
+            v-model="sizeForm.inputValue"
             ref="saveTagInput"
             size="mini"
             @keyup.enter.native="handleInputConfirm"
             @blur="handleInputConfirm"
           >
           </el-input>
-          <el-button
-            circle
-            type="danger"
-            v-else
-            class="button-new-tag"
-            size="small"
-            @click="showInput"
-            >+</el-button
+          <el-tag
+            closable
+            :disable-transitions="false"
+            :key="tag"
+            v-for="tag in sizeForm.dynamicTags"
+            @close="handleClose(tag)"
           >
-          <p>Add new</p>
+            {{ tag }}
+          </el-tag>
         </el-form-item>
       </div>
+      <!-- date -->
       <div class="grid">
-        <el-form-item label="Day">
-          <el-select v-model="value" placeholder="Select">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+        <div class="group">
+          <!-- day -->
+          <el-form-item label="Day" v-if="available">
+            <el-date-picker
+              v-model="sizeForm.date1"
+              type="date"
+              v-for="date1 in sizeForm.dynamicDateTime"
+              :key="date1"
+              ref="saveDate1"
+              placeholder="Pick a date"
+              default-value="2010-10-01"
+              @keyup.enter.native="handleavailableConfirm"
+              @blur="handleavailableConfirm"
             >
-            </el-option>
-          </el-select>
-          <el-select v-model="value" placeholder="Select">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
-            </el-option>
-          </el-select>
-        </el-form-item>
+              <!--  -->
+            </el-date-picker>
+          </el-form-item>
+          <div class="time">
+            <!-- from -->
+            <el-form-item label="From" v-if="available">
+              <el-time-select
+                placeholder="Start time"
+                v-model="sizeForm.startTime1"
+                ref="savestartTime1"
+                v-for="startTime1 in sizeForm.dynamicDateTime"
+                :key="startTime1"
+                :picker-options="{
+                  start: '00:00',
+                  step: '00:30',
+                  end: '23:30',
+                }"
+                @keyup.enter.native="handleavailableConfirm"
+                @blur="handleavailableConfirm"
+              >
+                <!-- {{ startTime1.startTime1 }} -->
+              </el-time-select>
+            </el-form-item>
+            <!-- to -->
+            <el-form-item label="To" v-if="available">
+              <el-time-select
+                placeholder="End time"
+                v-model="sizeForm.endTime1"
+                ref="saveendTime1"
+                v-for="endTime1 in sizeForm.dynamicDateTime"
+                :key="endTime1"
+                :picker-options="{
+                  start: '00:00',
+                  step: '00:30',
+                  end: '23:30',
+                }"
+                @keyup.enter.native="handleavailableConfirm"
+                @blur="handleavailableConfirm"
+              >
+                {{ endTime1.endTime1 }}
+              </el-time-select>
+            </el-form-item>
+          </div>
+        </div>
       </div>
+      <el-button class="button-new-tag" size="small" @click="showAvailableTime"
+        >+ New time
+      </el-button>
 
       <el-form-item size="large">
         <el-button @click="onSubmit">Save</el-button>
@@ -113,7 +139,7 @@ export default {
   data() {
     return {
       sizeForm: {
-        firstname: "",
+        irstname: "",
         lastname: "",
         dateOfBirth: "",
         gender: "1",
@@ -121,57 +147,76 @@ export default {
         state: "",
         city: "",
         address: "",
+        startTime1: "",
+        endTime1: "",
+        date1: "",
+        dynamicTags: ["Tag 1", "Tag 2", "Tag 3"],
+        dynamicDateTime: [
+          { date1: "10/10/2021", startTime1: "8:00", endTime1: "9:00" },
+        ],
+        inputValue: "",
+        value: "",
       },
-      dynamicTags: ["Tag 1", "Tag 2", "Tag 3"],
-      inputVisible: false,
-      inputValue: "",
-      options: [
-        {
-          value: "Option1",
-          label: "Option1",
-        },
-        {
-          value: "Option2",
-          label: "Option2",
-        },
-        {
-          value: "Option3",
-          label: "Option3",
-        },
-        {
-          value: "Option4",
-          label: "Option4",
-        },
-        {
-          value: "Option5",
-          label: "Option5",
-        },
-      ],
-      value: "",
+      available: false,
     };
   },
   methods: {
     onSubmit() {
       console.log("submit!");
     },
+    // remove tags
     handleClose(tag) {
-      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+      console.log("tag");
+      this.sizeForm.dynamicTags.splice(
+        this.sizeForm.dynamicTags.indexOf(tag),
+        1
+      );
     },
-
+    //show search
     showInput() {
-      this.inputVisible = true;
       this.$nextTick(() => {
         this.$refs.saveTagInput.$refs.input.focus();
       });
     },
-
+    // save search -> tag
     handleInputConfirm() {
-      let inputValue = this.inputValue;
+      let inputValue = this.sizeForm.inputValue;
       if (inputValue) {
-        this.dynamicTags.push(inputValue);
+        this.sizeForm.dynamicTags.push(inputValue);
       }
-      this.inputVisible = false;
       this.inputValue = "";
+    },
+
+    // show datetime
+    showAvailableTime() {
+      console.log("a");
+      this.available = true;
+      this.$refs.saveDate1.$el.focus();
+      this.$refs.savestartTime1.$el.focus();
+      this.$refs.saveendTime1.$el.focus();
+    },
+    // save date in input -> date
+    handleavailableConfirm() {
+      let inputDate1 = this.sizeForm.date1;
+      let inputStartTime1 = this.sizeForm.startTime1;
+      let inputEndTime1 = this.sizeForm.endTime1;
+
+      if (inputDate1 != "" && inputStartTime1 != "" && inputEndTime1 != "") {
+        let obj = {
+          date1: inputDate1,
+          startTime1: inputStartTime1,
+          endTime1: inputEndTime1,
+        };
+        if (obj) {
+          this.sizeForm.dynamicDateTime.push(obj);
+        }
+        console.log(this.sizeForm.dynamicDateTime);
+        // this.available = false;
+        inputStartTime1 = "";
+        inputEndTime1 = "";
+        inputDate1 = "";
+      }
+      // console.log(inputDate1);
     },
   },
 };
@@ -179,15 +224,21 @@ export default {
 
 <style scoped lang="scss">
 @import "../assets/scss/variables.scss";
+// .el-form{
+//   height: 100%;
+// }
 // tag
 .el-tag + .el-tag {
   margin-left: 10px;
   margin-top: 12px;
+  background-color: $btnColor;
+  color: $inputColor;
 }
-.el-button.button-new-tag.el-button--danger.el-button--small.is-circle {
-  width: 32px;
-  height: 32px;
-  margin-left: 10px;
+.el-button.button-new-tag {
+  width: 100px;
+  // height: 32px;
+  // margin-left: 10px;
+  margin-bottom: 15px;
 }
 // .button-new-tag {
 //   margin-left: 10px;
@@ -206,7 +257,7 @@ export default {
   display: flex;
   //   justify-content: ;
   .el-date-editor {
-    width: 340px;
+    width: 352px;
   }
 }
 .el-form-item {
@@ -216,7 +267,7 @@ export default {
   }
 }
 /deep/.el-input {
-  width: 340px;
+  width: 352px;
   .el-input__inner {
     height: 44px;
   }
@@ -229,10 +280,23 @@ export default {
   height: 40px;
   border: 1px solid $btnColor;
   background-color: $btnColor;
+  color: $inputColor;
 }
-.grid{
-    display: flex;
-    flex-direction: column;
-    background-color: violet;
+.grid {
+  // display: flex;
+  // flex-direction: column;
+  // background-color: violet;
+  /deep/.el-input {
+    width: 191px;
+    .el-input__inner {
+      height: 44px;
+    }
+  }
 }
+.time {
+  display: flex;
+  margin-left: 107px;
+}
+
+// .el-form-item__content{}
 </style>
